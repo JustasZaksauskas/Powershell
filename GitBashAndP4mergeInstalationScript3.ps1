f957966da8ecb968a15c84efd9649f0f6b227af5
JustasZaksauskas <zaksauskas.justas@gmail.com>
2017-11-27 09:02:58 +0200

    Add files via upload

---
+++ GitBashAndP4mergeInstalationScript3.ps1
@@ -0,0 +1,5 @@
+﻿New-Item c:\Downloads -type directory
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.15.0.windows.1/Git-2.15.0-64-bit.exe" -OutFile "C:\Downloads\Git-2.15.0-64-bit.exe"
+Invoke-WebRequest -Uri "\\arke\NAV Team\INSTALL\P4Merge\p4vinst.exe" -OutFile "C:\Downloads\p4vinst.exe"
+Start-Process -Wait -FilePath "C:\Downloads\Git-2.15.0-64-bit.exe" -ArgumentList '/SP- /VERYSILENT /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS '
+Start-Process -Wait -FilePath "C:\Downloads\p4vinst.exe" -ArgumentList '/S /V"/qn ALLUSERS=1 REBOOT=ReallySuppress"' 
---
+++ Notepad++_P4mergeInstalationScript3.ps1
@@ -0,0 +1,14 @@
+﻿$dir = Test-Path C:\Downloads 
+If (-not $dir){
+    New-Item c:\Downloads -type directory
+    }
+$notepadDir = Test-Path C:\Downloads\npp.7.5.1.Installer.x64.exe
+If(-not $notepadDir){
+    Invoke-WebRequest -Uri "https://notepad-plus-plus.org/repository/7.x/7.5.1/npp.7.5.1.Installer.x64.exe" -OutFile "C:\Downloads\npp.7.5.1.Installer.x64.exe"
+    }
+$p4Dir = Test-Path C:\Downloads\p4vinst.exe
+If(-not $p4Dir){
+    Invoke-WebRequest -Uri "\\arke\NAV Team\INSTALL\P4Merge\p4vinst.exe" -OutFile "C:\Downloads\p4vinst.exe"
+    }
+Start-Process -Wait -FilePath "C:\Downloads\p4vinst.exe" -ArgumentList '/S /V"/qn ALLUSERS=1 REBOOT=ReallySuppress"' 
+Start-Process -Wait -FilePath "C:\Downloads\npp.7.5.1.Installer.x64.exe" -ArgumentList '/S /V"/qn "' 
